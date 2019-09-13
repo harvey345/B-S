c5247e59144495dacebca82658f52ae6541a7124
Harvey <s1040080@korrnell.hcc.edu.tw>
2019-09-13 18:04:28 +0800

    Add new file

---
+++ BS.py
@@ -0,0 +1,8 @@
+num1=float(input("請輸入數字 :　"))
+num2= float(input("請輸入數字　：　"))
+if num1>num2:
+    print(num1,">",num2)
+if num1<num2:
+    print(num1,"<",num2)
+if num1=num2:
+    print(num1,"=",num2)
